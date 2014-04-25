22fcfb559d3dd8251bf5bae9b8256711d71d1c6d
vanjuxin-dju <sokol.vdu@gmail.com>
2014-04-25 08:25:13 +0300

    Клиентская часть работает с серверной

--- client/engine.js
+++ client/engine.js
@@ -3,6 +3,7 @@ var lastKey = 0;
 var keys = [];
 var values = [];
 var page = 1;
+var doUpdate = false;
 
 function currentDateString() {
 	var date = new Date();
@@ -35,6 +36,7 @@ $(document).ready(function () {
 
 	$('#removeall').on("click", function () {
 		$('.items').remove();
+		$.post("/remove", { 'id': 'all'});
 		length = lastKey = 0;
 	});
 
@@ -42,21 +44,56 @@ $(document).ready(function () {
 		id = $("#id_field").text();
 		name = $("#name_field").val();
 		description = $("#description_field").val();
+		date = $('#date_field').text();
 		isComplete = $("#complete_field").prop("checked");
-		if (/*!localStorage[id]*/ true) {
+		if (!doUpdate) {
 			date = currentDateString();
-			//jsonString = '{"id":"'+ id +'", "name":"' + name +'","description":"' + description + '","date":"'+date+'","iscomplete":"'+isComplete+'"}';
-			//localStorage.setItem(id, jsonString);
 			addElem(id, name, description, date, isComplete);
 			hide();
 			length++;
 			lastKey++;
-		} 
+		} else {
+			var inner = "<span>" + id + "</span> <span>" + name + "</span> <span>" + description + "</span> " +
+		    "<span>" + date + "</span> <span>" + isComplete + "</span> " +
+			"<span><img src='icons/edit.png' alt='Edit' title='Изменить' class='edit' onclick='updateTask(\"" + id + 
+			"\")'>" + "<img src='icons/remove.png' alt='Remove' title='Удалить' class='remove' onclick='removeTask(\"" + 
+			id + "\")'></span>";
+			var posting = $.post('/update', { 'id': id, 'name': name, 'description': description, 'date': date, 
+				'iscomplete': isComplete });
+		    posting.done(function (data) {
+		    	$('#progress').css('display', 'none');
+		    });
+		    $('#' + id).html(inner);
+		   	/*$('<div class="items" id="' + id + '" draggable>' + inner + 
+		   		'</div>').insertBefore(document.getElementById('add'));*/
+			doUpdate = false;
+			hide();
+		}
 	});
 
 	$('#hide').on("click", function () { 
 		hide();
 	});
+
+	var posting = $.post('/all', { 'fun': "getAllTasks" }, function(data) {
+		for (var i = 0; i < data.result.length; i++) {
+			if (data.result[i] == undefined)
+				continue;
+
+			var inner = "<span>" + data.result[i].ID + "</span> <span>" + data.result[i].Name + "</span> <span>" + 
+				data.result[i].Description + "</span> " + "<span>" + data.result[i].Date + "</span> <span>" + 
+				(data.result[i].Completed == 1 ? "true" : "false") + "</span> " +
+				"<span><img src='icons/edit.png' alt='Edit' title='Изменить' class='edit' onclick='updateTask(\"" + 
+				data.result[i].ID + "\")'>" + 
+				"<img src='icons/remove.png' alt='Remove' title='Удалить' class='remove' onclick='removeTask(\"" + 
+				data.result[i].ID + "\")'></span>";
+			$('<div class="items" id="' + data.result[i].ID + '" draggable>' + inner + 
+				'</div>').insertBefore(document.getElementById('add'));
+			length++;
+			var cur = parseInt(data.result[i].ID.substring(2));
+			lastKey = (lastKey < cur ? cur : lastKey);
+		};
+	}, "json");
 });
 
 /*function confirm() {
@@ -89,12 +126,13 @@ function hide() {
 	$("#name_field").val("");
 	$("#description_field").val("");
 	$("#complete_field").prop("checked", false);
+	$('#date_field').text('will be generated');
 	$('#add').css("display","none");
 }
 
 function removeTask(id) {
-	//localStorage.removeItem(id);
 	$("#" + id).remove();
+	$.post("/remove", { 'id': id});
 	length--;
 }
 
@@ -102,9 +140,15 @@ function updateTask(id) {
 	$('#add').css("display","block");
 	$("#whattodo").text("Изменить");
 	$("#id_field").text(id);
+	doUpdate = true;
+	// $('#' + id + " span:nth-child(1)").text();
 	//elem = JSON.parse(localStorage.getItem(id));
-	$("#name_field").val(elem.name);
-	$("#description_field").val(elem.description);
-	$("#date_field").text(elem.date);
-	$("#complete_field").prop("checked", (elem.iscomplete == "true" ? true : false));
+	$("#name_field").val($('#' + id + " span:nth-child(2)").text());
+	//$("#name_field").val(elem.name);
+	$("#description_field").val($('#' + id + " span:nth-child(3)").text());
+	//$("#description_field").val(elem.description);
+	$("#date_field").text($('#' + id + " span:nth-child(4)").text());
+	//$("#date_field").text(elem.date);
+	$("#complete_field").prop("checked", ($('#' + id + " span:nth-child(4)").text() == "true" ? true : false));
+	//$("#complete_field").prop("checked", (elem.iscomplete == "true" ? true : false));
 }
--- server.js
+++ server.js
@@ -13,40 +13,97 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'client')));
 
-/*
-var mysql      = require('mysql');
+var mysql = require('mysql');
+
+var result = [];
+
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret'
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'task_organiser'
 });
 
 connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+connection.query("select * from tasks", function (err, results) {
+	if (err) throw err;
+	for (var i = 0; i < results.length; i++) {
+		result.push(results[i]);
+	};
 });
 
-connection.end();
-*/
 app.get('/', function(req, res){
-    /*var body = 'rewrewr';
-	res.setHeader("Content-Type","text-plain");
-	res.setHeader("Content-Length", Buffer.byteLength(body));
-	res.end(body);*/
-	//app.get('client')
 	res.redirect('/index.html');
 });
+
 app.post("/addnote", function(req, res){
 	var id = req.body.id;
 	var name = req.body.name;
 	var description = req.body.description;
 	var date = req.body.date;
 	var iscomplete = req.body.iscomplete;
+
+	connection.query("insert into tasks (ID, Name, Description, Date, Completed) values ('" + id + 
+		"', "+connection.escape(name)+", "+connection.escape(description)+", '"+date+"', "+iscomplete+")");
 	
+	result.push({
+		'Completed': (iscomplete == "true" ? 1 : 0),
+		'Date': date,
+		'Description': description,
+		'ID': id,
+		'Name': name
+	});
+
 	res.json({ ok: true });
 	console.log("task " + id + " had added");
-})
+});
+
+app.post("/all", function (req, res) {
+	res.json({ 'result': result });
+});
+
+app.post("/remove", function(req, res) {
+	var id = req.body.id;
+
+	if (id == 'all') {
+		result = [];
+		connection.query("delete from tasks");
+		console.log("all tasks removed");
+	} else {
+		connection.query("delete from tasks where ID = '" + id + "'");
+		for (var i = 0; i < result.length; i++) {
+			if (result[i] != undefined && result[i].ID == id) {
+				result[i] = undefined;
+				console.log("task " + id + " removed");
+				break;
+			}
+		};
+	}
+});
+
+app.post("/update", function(req,res) {
+	var id = req.body.id;
+	var name = req.body.name;
+	var description = req.body.description;
+	var date = req.body.date;
+	var iscomplete = req.body.iscomplete;
+
+	connection.query("update tasks set Name = " + connection.escape(name) + ", Description = " + 
+		connection.escape(description) + ", Date = '" + date + "', Completed = " + iscomplete + 
+		" where ID = '" + id + "'");
+	
+	for (var i = 0; i < result.length; i++) {
+		if (result[i].ID == id) {
+			result[i].Name = name;
+			result[i].Description = description;
+			result[i].Date = date;
+			result[i].Completed = (iscomplete ? 1 : 0);
+			break;
+		}
+	};
+
+	res.json({ ok: true });
+	console.log("task " + id + " had updated");
+});
+
 app.listen(3000);
